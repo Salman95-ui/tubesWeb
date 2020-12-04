170e363bdb82eb9e50d1cab20e80c52ae646d9a7
Imaduddin00 <muhammad.118140010@student.itera.ac.id>
2020-12-04 23:30:37 +0700

    menambah tabel baru dengan migration

---
+++ database/migrations/2020_07_28_052019_create_lapor_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lapor', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('judul');
+            $table->string('komentar');
+            $table->string('aspek');
+            $table->string('file');
+            $table->dateTime('waktu');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lapor');
+    }
+}
